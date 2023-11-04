b77e0b72285e5a7197dd3ffebb73190bbdc7c86a
anthena0 <apple930615@gmail.com>
2023-11-05 02:10:25 +0800

    1105hw

---
+++ p18/p18/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	char stringl[60]="Welcome";
+	char string2[60];
+
+	int len;
+
+	strcpy(string2, stringl);
+	printf("string2=%s\n", string2);
+
+	len=strlen(string2);
+	printf("¦r¦êªø«×¬°%d\n", len);
+
+	system("pause");
+	return 0;
+}
